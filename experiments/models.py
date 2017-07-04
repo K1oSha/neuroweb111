466a55e2de7ee6836b9352d32e63dbe09e26a350
egorrridze <gor7501@mail.ru>
2017-07-04 16:25:41 +1000

    fix models.py

--- experiments/models.py
+++ experiments/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Participant(models.Model):
     first_name = models.CharField(max_length=60)
     last_name = models.CharField(max_length=60)
@@ -7,9 +8,11 @@ class Participant(models.Model):
     physical_data = models.TextField()
     bio = models.TextField()
     is_female = models.BooleanField()
-    avatar_url = models.ImageField(upload_to = 'participant_photos/', default = 'participant_photos/no-img.jpg')
+    avatar_url = models.ImageField(upload_to='participant_photos/', default='participant_photos/no-img.jpg')
     test_list = models.TextField()
-#https://coderwall.com/p/bz0sng/simple-django-image-upload-to-model-imagefield
+
+
+# https://coderwall.com/p/bz0sng/simple-django-image-upload-to-model-imagefield
 class Experiments(models.Model):
     assistant = models.ForeignKey(
         Assistant,
@@ -22,9 +25,12 @@ class Experiments(models.Model):
     process = models.TextField()
     results = models.CharField(max_length=128)
     date = models.DateField()
+
+
 class Assistant(models.Model):
     first_name = models.CharField(max_length=60)
     last_name = models.CharField(max_length=60)
     age = models.IntegerField(max_length=3)
     position = models.CharField(max_length=128)
+
 # Create your models here.
